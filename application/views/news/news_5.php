bb3a8e4bbbc043de87469e7e9e57668ec2fb760a
DELL <DELL@172.16.184.205>
2018-03-05 18:27:37 +0700

    ข่าว

---
+++ application/views/news/news_5.php
@@ -0,0 +1,23 @@
+<section class="classic-blog-section section">
+    <div class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+                <article class="blog-post-wrapper wow fadeIn animated" data-wow-delay="0.3s" style="visibility: visible;-webkit-animation-delay: 0.3s; -moz-animation-delay: 0.3s; animation-delay: 0.3s;">
+                    <header class="author-info">
+                        <h2 class="blog-post-title">Oracle Academy และ สำนักวิชาสารสนเทศศาสตร์ จัดอบรมฟรี</h2>
+        				</header>
+        				<section class="featured-wrapper_de">
+            				<a href="#">
+            				 <img src="<?php echo base_url()?>assets/img/news/oracle_news.jpg" alt=""> 			</a>
+        				</section>
+        				<section class="blog-post-content">
+        					<div class="blog-post">
+        						<p> Oracle Academy บริษัทออราเคิลคอร์ปอเรชั่น(ประเทศไทย) จำกัด กับทางหลักสูตรเทคโนโลยีสารสนเทศ สํานักวิชาสารสนเทศศาสตร์ มหาวิทยาลัยวลัยลักษณ์ และทีมงาน Thaitechnet  จุดประสงค์ของการอบรมเพื่อพัฒนาศักยภาพของนักศึกษาให้มีความรู้ความเข้าใจ และมีทักษะในการใช้เทคโนโลยีของ Oracle อันจะเป็นประโยชน์ต่อการนำไปประกอบวิชาชีพในอนาคต 
+</p>
+        					</div>
+        				</section>
+    				</article>
+    			</div>
+    		</div>
+    </div>
+</section>
